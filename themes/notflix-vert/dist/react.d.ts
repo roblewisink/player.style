7ee44dbe5dc03b01159a593edda1d9d9e3a311f1
roblewisink <38957770+roblewisink@users.noreply.github.com>
2025-05-14 00:54:28 +0000

    Clean

--- themes/notflix-vert/dist/react.d.ts
+++
@@ -1,7 +0,0 @@
-import MediaThemeNotflixVertElement from './media-theme.js';
-
-declare const MediaThemeNotflixVert: React.ForwardRefExoticComponent<
-  React.DetailedHTMLProps<React.HTMLAttributes<MediaThemeNotflixVertElement>, MediaThemeNotflixVertElement>
->;
-
-export default MediaThemeNotflixVert;
